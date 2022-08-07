8f16873af9a71ceee82989ae517987b954050670
Compass <ndphong165@gmail.com>
2022-08-08 00:09:58 +0700

    up thuc hanh 1 Array

---
+++ ArrayOfJava/src/Test/Test.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class Test {
+
+    public static void main(String[] args) {
+        double mylist[] = {1,2,3,4,5,6,7,8,9,10};
+
+        for (double a: mylist) {
+            System.out.print(a + "\t");
+        }
+    }
+
+}
