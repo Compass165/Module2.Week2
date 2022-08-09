987a92a11f40d0f94127d54bede5f6d7b7aeae6e
Compass <ndphong165@gmail.com>
2022-08-09 13:36:28 +0700

    Up StopWatch

--- ObjAndClass/StopWatch/src/StopWatch/StopWatch.java
+++ ObjAndClass/StopWatch/src/StopWatch/StopWatch.java
@@ -1,4 +1,46 @@
 package StopWatch;
 
+import java.time.LocalTime;
+import java.time.temporal.Temporal;
+
 public class StopWatch {
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    public LocalTime getStartTime(LocalTime startTime) {
+        return startTime;
+    }
+
+    public LocalTime getEndTime(LocalTime endTime) {
+
+        return endTime;
+    }
+
+    StopWatch() {
+        startTime = LocalTime.now();
+    }
+
+    public void start() {
+        this.startTime = LocalTime.now();
+        getStartTime(startTime);
+    }
+
+    public void stop() {
+        this.endTime = LocalTime.now();
+        getEndTime(endTime);
+    }
+
+    public void getElapsedTime() {
+        int ElaspedTime = (startTime.toSecondOfDay() - endTime.toSecondOfDay()) * 100;
+        System.out.println("Số mili giây đếm đc:" + ElaspedTime);
+    }
+
+    public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        System.out.println("Đang đếm thời gian");
+        for (int i = 1; i <= 1000000; i++)
+            stopWatch.stop();
+        stopWatch.getElapsedTime();
+    }
 }
